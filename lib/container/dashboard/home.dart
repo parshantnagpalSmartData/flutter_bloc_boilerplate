b1ecb90c52b682e8b0a8faffae2843931d09b53a
Ravi kumar <ravi@smartdata.com>
2020-03-14 12:08:57 +0530

    boilerplate

---
+++ lib/container/dashboard/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../auth/login.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (_, count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () =>
+                  context.bloc<CounterBloc>().add(CounterEvent.increment),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () =>
+                  context.bloc<CounterBloc>().add(CounterEvent.decrement),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.update),
+              onPressed: () => {
+                // context.bloc<CounterBloc>().add(CounterEvent.decrement),
+                // context.bloc<ThemeBloc>().add(ThemeEvent.toggle)
+                       Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                  )
+                },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
+  }
+}
+
+enum ThemeEvent { toggle }
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
+  @override
+  ThemeData get initialState => ThemeData.light();
+
+  @override
+  Stream<ThemeData> mapEventToState(ThemeEvent event) async* {
+    switch (event) {
+      case ThemeEvent.toggle:
+        yield state == ThemeData.dark() ? ThemeData.light() : ThemeData.dark();
+        break;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'container/auth/login.dart';
+import 'container/dashboard/home.dart';
+
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
+}
+
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeData>(
+        builder: (_, theme) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            home: BlocProvider(
+              create: (_) => CounterBloc(),
+              child: Home(),
+              //  child:Login(title: 'Flutter Login'),
+            ),
+            theme: theme,
+          );
+        },
+      ),
+    );
+  }
+}
+
+enum ThemeEvent { toggle }
+class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
+  @override
+  ThemeData get initialState => ThemeData.light();
+
+  @override
+  Stream<ThemeData> mapEventToState(ThemeEvent event) async* {
+    switch (event) {
+      case ThemeEvent.toggle:
+        yield state == ThemeData.dark() ? ThemeData.light() : ThemeData.dark();
+        break;
+    }
+  }
+}
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
+  }
+}
